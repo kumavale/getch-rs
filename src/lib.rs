746ef54868a3c9b7bf0466823750e5ebfed7996e
kumavale <kumavale.hyperion@gmail.com>
2024-12-31 16:24:08 +0900

    chore: clippy

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,15 @@
 //! ```no_run
 //! use getch_rs::{Getch, Key};
 //!
-//! fn main() {
-//!     let g = Getch::new();
+//! let g = Getch::new();
 //!
-//!     println!("press `q` to exit");
+//! println!("press `q` to exit");
 //!
-//!     loop {
-//!         match g.getch() {
-//!             Ok(Key::Char('q')) => break,
-//!             Ok(key) => println!("{:?}", key),
-//!             Err(e) => println!("{}", e),
-//!         }
+//! loop {
+//!     match g.getch() {
+//!         Ok(Key::Char('q')) => break,
+//!         Ok(key) => println!("{:?}", key),
+//!         Err(e) => println!("{}", e),
 //!     }
 //! }
 //! ```
